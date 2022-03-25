f16aa95838e935f2dcde2361e347436f1f2af1c1
Martha Quintanilla-Ramirez <marthaquintanilla-ramirez@Marthas-MacBook-Pro.local>
2022-03-24 21:38:12 -0700

    updated some code on my js, added .shift

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@
 // console.log(allProduct);
 
 
-
-
 // let images = ["img/bag.jpeg", "img/banana.jpeg", "img/bathroom.jpeg", "img/boots.jpeg", "img/breakfast.jpeg", "img/bubblegum.jpeg", "img/chair.jpeg", "img/cthulhu.jpeg", "img/dog-duck.jpeg", "img/dragon.jpeg", "img/pen.jpeg", "img/pet-sweep.jpeg", "img/scissors.jpeg", "img/shark.jpeg", "img/tauntaun.jpeg", "img/unicorn.jpeg", "img/water-can.jpeg", "img/win-glass.jpeg", "img/sweep.png"];
 // console.log(images);
+
+
 
 
 
@@ -80,32 +80,37 @@ function getRandomNumber() {
 function renderImages() {
   let uniqueProductIndexes = [];// this is for randomized images.
 
-  while (uniqueProductIndexes.length < 3) {
+  while (uniqueProductIndexes.length < 6) {
     let num = getRandomNumber();
-    while (uniqueProductIndexes.includes(num)) {
+    if (!uniqueProductIndexes.includes(num)) {
       num = getRandomNumber();
     }
     uniqueProductIndexes.push(num);
   }
 
+console.log(uniqueProductIndexes);
+let index1 = uniqueProductIndexes.shift(); //this removes the 1st item from the front of the array, this must reorder indexes
+let index2 = uniqueProductIndexes.shift();
+let index3 = uniqueProductIndexes.shift();
 
 
-  imageOne.src = allProducts[uniqueProductIndexes[0]].src; // this is to access the index of the unique products index array, this is getting the first number in that array.
-  imageOne.alt = allProducts[uniqueProductIndexes[0]].name;// this is naming it
-  allProducts[uniqueProductIndexes[0]].views++;// this is incrementing  the amount of times the images have been viewed, we have a view property inside of the constructor.
+imageOne.src = allProducts[index1].src; // this is to access the index of the unique products index array, this is getting the first number in that array.
+imageOne.alt = allProducts[index1].name;// this is naming it
+allProducts[index1].views++;// this is incrementing  the amount of times the images have been viewed, we have a view property inside of the constructor.
 
 
-  imageTwo.src = allProducts[uniqueProductIndexes[1]].src;
-  imageTwo.alt = allProducts[uniqueProductIndexes[1]].name;
-  allProducts[uniqueProductIndexes[1]].views++;
+imageTwo.src = allProducts[index2].src; // this is adding in a new
+imageTwo.alt = allProducts[index2].name;
+allProducts[index2].views++;
 
-  imageThree.src = allProducts[uniqueProductIndexes[2]].src;
-  imageThree.alt = allProducts[uniqueProductIndexes[2]].name;
-  allProducts[uniqueProductIndexes[2]].views++;
+imageThree.src = allProducts[index3].src;
+imageThree.alt = allProducts[index3].name;
+allProducts[index3].views++;
 
-
-  // console.log(uniqueProductIndexes);
 }
+renderImages();
+// console.log(uniqueProductIndexes);
+
 
 
 
@@ -119,29 +124,28 @@ function handlerImageClick(event) {
 
 
   for (let i = 0; i < allProducts.length; i++) { // this is incrementing the clicks. we are going through our product array and when we know we got the right product itll increment that thing only.
-    if (allProducts[i].name === imageClicked) {// if all product at i is strictly equal then we have know we have the right one.
+    if (allProducts[i].name === imageClicked) // if all product at i is strictly equal then we have know we have the right one.
       allProducts[i].votes++;//votes is property of my constructor
+  }
+    if (clicksMade === ATTEMPTS_ALLOWED) {
+      imageContainer.removeEventListener('click', handlerImageClick);// this is removing the event  listener and the call back functions so when we hit 25 clicks it stops.
     }
-  }
-  renderImages();
-
-  if (clicksMade === ATTEMPTS_ALLOWED) {
-    imageContainer.removeEventListener('click', handlerImageClick);// this is removing the event  listener and the call back functions so when we hit 25 clicks it stops.
-  }
-
+    renderImages();
+ 
 }
 
 
 
 
 //event handler for showing results
-function handleResults(event) {
+function handleResults() {
   if (clicksMade === ATTEMPTS_ALLOWED) {
     for (let i = 0; i < allProducts.length; i++) {
       let li = document.createElement('li');// create a list
       li.textContent = `${allProducts[i].name} had ${allProducts[i].votes} votes, and was seen ${allProducts[i].views} times`; // this is giving text contents
       results.appendChild(li); // this is showing it on the webpage. but its not  working.
     }
+    showResults.removeEventListener('click', handleResults);
   }
 }
 
@@ -150,7 +154,7 @@ function handleResults(event) {
 // //executable code
 // // instantiate products
 // // call the function to do things?
-renderImages();
+// renderImages();
 
 
 
@@ -163,3 +167,47 @@ imageContainer.addEventListener('click', handlerImageClick)//this is an arguemen
 // //listen to "div" button to show results
 showResults.addEventListener('click', handleResults);
 // };
+
+// function renderChart(){
+//   let views = [];
+//   let votes = [];
+//   let names = [];
+
+//   for (let i = 0; i < allProducts.length; i++){
+//   let product = allProducts[i];
+//   views.push(product.views);
+//   votes.push(product.votes);
+//   names.push(product.name);
+// }
+
+let config = {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Views',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: 'rgb(80, 206, 100, .5)',
+      borderColor: 'rgba(80, 162, 235, 1)',
+      borderWidth: 1
+    },
+    {
+      label: '# of votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: 'rgba(280, 110, 106, .8)',
+      borderColor: 'rgba(75, 192, 192, 3)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    //  responsive: true
+  }
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, config);
