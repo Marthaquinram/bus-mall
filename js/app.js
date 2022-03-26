b50e0431240010d7fbc64d1b3130dee5b7e77e3f
Martha Quintanilla-Ramirez <marthaquintanilla-ramirez@Marthas-MacBook-Pro.local>
2022-03-25 20:19:16 -0700

    updated my code to display different photos and display the labels.

--- js/app.js
+++ js/app.js
@@ -77,35 +77,34 @@ function getRandomNumber() {
   return Math.floor(Math.random() * allProducts.length);// the max is exclusive and the min is inclusive
 }
 // render images - in one function,
+let uniqueProductIndexes = [];// this is for randomized images.
 function renderImages() {
-  let uniqueProductIndexes = [];// this is for randomized images.
-
   while (uniqueProductIndexes.length < 6) {
     let num = getRandomNumber();
-    if (!uniqueProductIndexes.includes(num)) {
+    while (uniqueProductIndexes.includes(num)) {// while is going to run as many times as it needs to until whatevers in () is false. is this thing true if it is keep running the while loop.
       num = getRandomNumber();
     }
     uniqueProductIndexes.push(num);
   }
 
-console.log(uniqueProductIndexes);
-let index1 = uniqueProductIndexes.shift(); //this removes the 1st item from the front of the array, this must reorder indexes
-let index2 = uniqueProductIndexes.shift();
-let index3 = uniqueProductIndexes.shift();
+  console.log(uniqueProductIndexes);
+  let index1 = uniqueProductIndexes.shift(); //this removes the 1st item from the front of the array, this must reorder indexes
+  let index2 = uniqueProductIndexes.shift();
+  let index3 = uniqueProductIndexes.shift();
 
 
-imageOne.src = allProducts[index1].src; // this is to access the index of the unique products index array, this is getting the first number in that array.
-imageOne.alt = allProducts[index1].name;// this is naming it
-allProducts[index1].views++;// this is incrementing  the amount of times the images have been viewed, we have a view property inside of the constructor.
+  imageOne.src = allProducts[index1].src; // this is to access the index of the unique products index array, this is getting the first number in that array.
+  imageOne.alt = allProducts[index1].name;// this is naming it
+  allProducts[index1].views++;// this is incrementing  the amount of times the images have been viewed, we have a view property inside of the constructor.
 
 
-imageTwo.src = allProducts[index2].src; // this is adding in a new
-imageTwo.alt = allProducts[index2].name;
-allProducts[index2].views++;
+  imageTwo.src = allProducts[index2].src; // this is adding in a new
+  imageTwo.alt = allProducts[index2].name;
+  allProducts[index2].views++;
 
-imageThree.src = allProducts[index3].src;
-imageThree.alt = allProducts[index3].name;
-allProducts[index3].views++;
+  imageThree.src = allProducts[index3].src;
+  imageThree.alt = allProducts[index3].name;
+  allProducts[index3].views++;
 
 }
 renderImages();
@@ -127,11 +126,11 @@ function handlerImageClick(event) {
     if (allProducts[i].name === imageClicked) // if all product at i is strictly equal then we have know we have the right one.
       allProducts[i].votes++;//votes is property of my constructor
   }
-    if (clicksMade === ATTEMPTS_ALLOWED) {
-      imageContainer.removeEventListener('click', handlerImageClick);// this is removing the event  listener and the call back functions so when we hit 25 clicks it stops.
-    }
-    renderImages();
- 
+  if (clicksMade === ATTEMPTS_ALLOWED) {
+    imageContainer.removeEventListener('click', handlerImageClick);// this is removing the event  listener and the call back functions so when we hit 25 clicks it stops.
+  }
+  renderImages();
+
 }
 
 
@@ -146,7 +145,9 @@ function handleResults() {
       results.appendChild(li); // this is showing it on the webpage. but its not  working.
     }
     showResults.removeEventListener('click', handleResults);
+  
   }
+  renderChart();
 }
 
 
@@ -168,46 +169,48 @@ imageContainer.addEventListener('click', handlerImageClick)//this is an arguemen
 showResults.addEventListener('click', handleResults);
 // };
 
-// function renderChart(){
-//   let views = [];
-//   let votes = [];
-//   let names = [];
+function renderChart() {
+  let views = [];// containers
+  let votes = [];
+  let names = [];
 
-//   for (let i = 0; i < allProducts.length; i++){
-//   let product = allProducts[i];
-//   views.push(product.views);
-//   votes.push(product.votes);
-//   names.push(product.name);
-// }
-
-let config = {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Views',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgb(80, 206, 100, .5)',
-      borderColor: 'rgba(80, 162, 235, 1)',
-      borderWidth: 1
-    },
-    {
-      label: '# of votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(280, 110, 106, .8)',
-      borderColor: 'rgba(75, 192, 192, 3)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    },
-    //  responsive: true
+  for (let i = 0; i < allProducts.length; i++) {
+    let product = allProducts[i];//products tht go into it(objects)
+    views.push(product.views);//Im pushing products views in to the views array.
+    votes.push(product.votes);//Im pushing products votes in to the votes array.
+    names.push(product.name);//Im pushing products name in to the names array.
   }
-};
-
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, config);
+  const ctx = document.getElementById('myChart').getContext('2d');
+  
+  let config = {
+    type: 'bar',
+    data: {
+      labels: names,
+      datasets: [{
+        label: '# of Views',
+        data: views,
+        backgroundColor: 'rgb(80, 206, 100, .5)',
+        borderColor: 'rgba(80, 162, 235, 1)',
+        borderWidth: 1
+      },
+      {
+        label: '# of votes',
+        data: votes,
+        backgroundColor: 'rgba(280, 110, 106, .8)',
+        borderColor: 'rgba(75, 192, 192, 3)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      //  responsive: true
+    }
+  };
+  
+  const myChart = new Chart(ctx, config);
+  
+}
